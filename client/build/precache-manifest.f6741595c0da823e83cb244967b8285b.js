c03c260b4a5ca30c47aaad3437e50d18aa9598a3
Adrian <murilloadrianc@gmail.com>
2020-02-26 12:37:00 -0300

    added build

---
+++ client/build/precache-manifest.f6741595c0da823e83cb244967b8285b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f75e443b3e71910b6203d31c580c32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c675e1130090cb5b5b9e",
+    "url": "/static/css/2.4121280a.chunk.css"
+  },
+  {
+    "revision": "f9b0c8715e6d85f4e1eb",
+    "url": "/static/css/main.fdbab4fd.chunk.css"
+  },
+  {
+    "revision": "c675e1130090cb5b5b9e",
+    "url": "/static/js/2.aa5e19b8.chunk.js"
+  },
+  {
+    "revision": "12570cd1c8f03b5daf36b1f621313989",
+    "url": "/static/js/2.aa5e19b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b0c8715e6d85f4e1eb",
+    "url": "/static/js/main.15c6257d.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
